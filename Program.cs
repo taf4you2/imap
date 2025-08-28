055ffd5d79835a18616239d22d3e4690e6ce04e5
Wojtek <carboncyclist2004@gmail.com>
2025-08-28 22:02:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Install-Package MailKit
+    Install-Package MimeKit
+ */
+
+using imap_samemu;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+
+
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static async Task main()
+    {
+        Console.WriteLine("=== Gmail Reader ===");
+
+
+
+
+    }
+}
