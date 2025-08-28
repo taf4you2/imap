f3f260e10ad9e2224f017b0eed6a9c6eaf868ebe
Wojtek <carboncyclist2004@gmail.com>
2025-08-28 22:05:24 +0200

    dodano do gitignore ignorowanie json bo tak

---
+++ json.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.Json;
+
+namespace imap_samemu
+{
+    public class json
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+    }
+}
