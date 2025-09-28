b36e0d3660b0e938e20d459dd432b17f6ed01307
wojtek <wojtek@DESKTOP-MSN5DM6>
2025-09-28 22:16:17 +0200

    cos tam pisze

---
+++ MessageCounter.cs
@@ -0,0 +1,44 @@
+﻿
+using Org.BouncyCastle.Bcpg;
+using System.Diagnostics;
+
+namespace imap_samemu
+{
+    internal class MessageCounter
+    {
+        private readonly string _CounterFilePath;
+
+        private int LastCount;
+
+        public MessageCounter (string CounterFilePath = "messege_counter.txt")
+        {
+            _CounterFilePath = CounterFilePath;
+            LoadCount();
+        }
+
+        public int CurrentCount => LastCount;
+
+        private void LoadCount()
+        {
+            try
+            {
+                if (File.Exists(_CounterFilePath))
+                {
+                    string content = File.ReadAllText(_CounterFilePath).Trim();
+
+                    if (int.TryParse(content, out int count))
+                    {
+                        LastCount = count;
+                        Console.WriteLine($"Wczytano ostatnią liczbę wiadomości: {_lastProcessedCount}");
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+        }
+
+
+    }
+}
