0cf99c1d863adecb2cd2c66c456e57979749c5d0
wojtek <wojtek@DESKTOP-MSN5DM6>
2025-10-03 21:17:02 +0200

    zaczynam zapisywac dane do plikow

---
+++ DataStorage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imap_samemu
+{
+    public static class DataStorage
+    {
+        private static readonly string BaseDirectory = "data";
+
+        private static string GetDataTypePath(DataType dataType)
+        {
+            string typeName = dataType.ToString().ToLower();
+            return Path.Combine(BaseDirectory, typeName);
+        }
+
+        public static void InicializeStorage()
+        {
+            if (!Directory.Exists(BaseDirectory))
+            {
+                Directory.CreateDirectory(BaseDirectory);
+                Console.WriteLine($"Utworzono katalog: {BaseDirectory}");
+            }
+
+
+            foreach (DataType dataType in Enum.GetValues<DataType>())
+            {
+                if (dataType == DataType.unknown) continue;
+
+                string typePath = GetDataTypePath(dataType);
+                if (!Directory.Exists(typePath))
+                {
+                    Directory.CreateDirectory(typePath);
+                    Console.WriteLine($"Utworzono katalog: {dataType}");
+                }
+            }
+
+        }
+
+        private static string GenerateFileName(DataType dataType, DateTime timestamp)
+        {
+            string typeName = dataType.ToString().ToLower();
+            string dateString = timestamp.ToString("yyyy-MM-dd");
+            return $"{typeName}_{dateString}.txt";
+        }
+
+
+
+
+
+
+    }
+}
